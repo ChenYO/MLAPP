6265c5676d9edc7c4e99baa608cbaf8d7efd341e
Chen Yao <loxa0802@gmail.com>
2018-09-20 11:33:20 +0800

    Initial Commit

---
+++ MLAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLAPP
+//
+//  Created by 陳仲堯 on 2018/9/20.
+//  Copyright © 2018年 陳仲堯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
